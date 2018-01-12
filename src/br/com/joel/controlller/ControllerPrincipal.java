18c253bab573692acbe32fb845413a18754464e8
joelh2000 <joelhenrique2000@gmail.com>
2018-01-11 23:40:20 -0200

    criei uma classe com nome Cifra no model

--- src/br/com/joel/controlller/ControllerPrincipal.java
+++ src/br/com/joel/controlller/ControllerPrincipal.java
@@ -8,34 +8,3 @@ public class ControllerPrincipal {
     
     
 }
-
-class cifra {
-    
-    Alfabeto alfabeto = new Alfabeto();
-    
-    public String cifrarTexto(String textoNormal, int chave) {
-        
-        String textoCifrado = "";
-        
-        for(int i=0; i<textoNormal.length(); i++) {
-            int numeroCifrado = alfabeto.procurarNumeroPelaLetra(textoNormal.charAt(i)) + chave;
-            textoCifrado = ""+ alfabeto.procurarLetraPeloNumero(numeroCifrado);
-        }
-        
-        return textoCifrado;
-    }
-    
-    public String decifrarTexto(String textoCifrado, int chave) {
-        
-        String textoNormal = "";
-        
-        for(int i=0; i<textoNormal.length(); i++) {
-            int numeroCifrado = alfabeto.procurarNumeroPelaLetra(textoNormal.charAt(i)) - chave;
-            textoCifrado = ""+ alfabeto.procurarLetraPeloNumero(numeroCifrado);
-        }
-        
-        return textoNormal;
-        
-    }
-    
-}
